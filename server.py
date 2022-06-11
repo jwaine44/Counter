db4d4e8a476d1f8cf506ac0b7cdd87fbaf6c228a
jwaine44 <jwaine44@gmail.com>
2022-06-11 17:03:42 -0400

    Changed server

--- server.py
+++ server.py
@@ -14,15 +14,15 @@ def start_counting():
     session['count'] += 1
     return redirect("/count")
 
-@app.route("/count_by_two", methods=['POST'])
-def count_by_two():
-    session['count'] += 2
-    return redirect("/count_by_two")
-
 @app.route("/count")
 @app.route("/count_by_two")
 def display_count():
     return render_template("index.html")
+
+@app.route("/count_by_two", methods=['POST'])
+def count_by_two():
+    session['count'] += 2
+    return redirect("/count_by_two")
 
 @app.route("/destroy_session", methods=['POST'])
 def clear_count():
